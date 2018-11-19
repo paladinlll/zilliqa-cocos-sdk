b19aacc7326807d27ec7bcfb0cba5a057aae6463
Phương Nguyễn <paladinlll.kun@gmail.com>
2018-11-19 15:06:59 +0700

    Add zilliqanetwork readme

--- deps/index.js
+++ deps/index.js
@@ -1,5 +1,6 @@
-const { Zilliqa } = require('@zilliqa-js/zilliqa')
-const BN = require('bn.js')
+const {BN} = require('@zilliqa-js/util');
+const {Zilliqa} = require('@zilliqa-js/zilliqa');
+
 export { 
   Zilliqa,
   BN
@@ -8,16 +9,3 @@ export {
 if (typeof window !== 'undefined' && typeof window.Zilliqa === 'undefined') {
   window.Zilliqa = Zilliqa;
 }
-
-/*
-
-const { Transaction } = require('@zilliqa-js/account')
-const { Zilliqa } = require('@zilliqa-js/zilliqa')
-const BN = require('bn.js')
-
-const zilliqa = new Zilliqa('https://my-zil-testnet.com');
-
-// Populate the wallet with an account
-address = zilliqa.wallet.addByPrivateKey('79A965ED6F516933838C4EC94D3B9512EB888DC02DC84115C40D274B7B76C99D');
-
-console.log('address', address);*/
--- sample/zilliqanetwork/README.md
+++ sample/zilliqanetwork/README.md
@@ -1,2 +1,29 @@
-# hello-world
-Hello world new project template.
+# zilliqanetwork
+zilliqanetwork simplely use zilliqa sdk
+
+# Swapper Funtions (ZilliqaNetwork.ts)
+* `getUserAddress
+* `wasAuthenticated
+* `connect
+* `getNetworkId
+* `loadKeyStore
+* `saveKeyStore
+* `generateNewAccount
+* `authorizeAccount
+* `sendFaucetRequest
+* `getBalance
+* `deployHelloWorld
+
+## Demo screen
+* `Connect
+![](./../../images/1_connect.png)
+
+* `Create account & Login
+![](./../../images/2a_new.png)
+![](./../../images/2b_login.png)
+
+* `Main
+![](./../../images/3_balance.png)
+
+* `Error
+![](./../../images/4_error.png)
